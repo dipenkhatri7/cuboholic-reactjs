eb7875d2ddf5b875ded91daf4f598ec1d977259d
erwinschordinger1 <sunnynirjal@gmail.com>
2022-01-10 20:33:10 +0545

    hero

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Nirjal');
 })
 
-app.post('/scramble', (req,res)=>{
-    
+app.get('/scramble', (req, res) => {
+    console.log("in scramble")
+    const moves = ['L', 'R', 'D', 'U', 'B', 'F']
+    const threeScramble = [' ', '2', '`'];
+    let scramble = "";
+    // let move1 = threeScramble[Math.random() * 6]
+    let move2;
+    let m1;
+    let minmoves = 16;
+    let maxmoves = 21;
+    // let scramble_len = minmoves + Math.random() * (maxmoves - minmoves);
+    for (let i = 0; i < scramble_len; i++) {
+        move2 = moves[Math.random() * moves.length];
+        if (move1 === move2) {
+            i--;
+        }
+        else {
+            m1 = threeScramble[Math.random() * threeScramble.length];
+            scramble += move2 + m1;
+            move1 = move2;
+        }
+    }
+    res.send(scramble);
 })
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log("Running in server 5000");
 })
