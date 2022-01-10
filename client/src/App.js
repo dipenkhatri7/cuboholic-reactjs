608e33beb935f70f7fdddcd97816c61b0cd5aca0
erwinschordinger1 <sunnynirjal@gmail.com>
2022-01-10 20:36:54 +0545

    hero

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import Nav from './components/navigation/nav'
+import Timer from './components/timerPage/timer'
+import CubeIcon from './components/cubeSelect/cubeIcon'
+// import Login from './components/login/login'
+// import Forget from './components/login/forget'
+// import Transition from './components/login/transition'
+// import SignUp from './components/login/signUp'
+// import NewPassword from './components/login/newPassword'
+// import CubeSelector from './components/cubeSelect/cubeSelector'
 
-function App() {
+
+function App(){
   
-  function generateScramble(ofwhich, minmoves, maxmoves) {
-    // let scramble = "";
-    // let move1 = ofwhich[Math.random()*6]
-    // let move2;
-    // let m1;
-    // let scramble_len = minmoves+Math.random()*(maxmoves-minmoves);
-    // for(let i=0 ; i<scramble_len;i++)
-    // {
-    //   move2 = moves[Math.random()*moves.length];
-    //   if(move1 === move2 )
-    //   {
-    //     i--;
-    //   }
-    //   else{
-    //     m1 = ofwhich[Math.random()*ofwhich.length];
-    //     scramble += move2 +m1;
-    //     move1 = move2;
-    //   }
-    // }
-    console.log("clicked")
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={(ofwhich) => { 
-          const moves = ['L', 'R', 'D', 'U', 'B', 'F']
-          const threeScramble = [' ', '2', '`'];  
-          let scramble = "";
-          let move1 = ofwhich[Math.random() * 6]
-          let move2;
-          let m1;
-          let scramble_len = minmoves + Math.random() * (maxmoves - minmoves);
-          for (let i = 0; i < scramble_len; i++) {
-            move2 = moves[Math.random() * moves.length];
-            if (move1 === move2) {
-              i--;
-            }
-            else {
-              m1 = ofwhich[Math.random() * ofwhich.length];
-              scramble += move2 + m1;
-              move1 = move2;
-            }
-          }
-        }}>Nirjal is </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+      <Nav title="CUBOHOLIC"/>  
+      <Timer /> 
+      <CubeIcon />
+      {/* <CubeSelector /> 
+      <Login />
+      <Forget />
+      <Transition />
+      <SignUp />
+      <NewPassword /> */}
+      
     </div>
+
   );
 }
 
-export default App;
+export default App
